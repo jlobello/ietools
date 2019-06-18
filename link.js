8b83bdad098c8af48a1bbb16befdf115c716b386
Jeff LoBello <jeffrey.lobello@cerner.com>
2019-06-17 20:40:43 -0400

    New build with zoom API

--- link.js
+++ link.js
@@ -1,5 +1,5 @@
 function fixLink()
 {
 var mylink = document.getElementById("mylink");
-mylink.href = "IEToolsChromeBootstrapper-4.3.200.784-20190412.175100-1.exe";
+mylink.href = "IEToolsChromeBootstrapper-4.3.200.936-20190617.204012-1.exe";
 }
